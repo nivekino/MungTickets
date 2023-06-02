078c826ad982521f3d1cb3dce263558ab1e6ca8e
nivekino <00100719@uca.edu.sv>
2023-06-02 15:23:27 -0600

    update fix

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./assets/footer.css";
+import logo from "./assets/img/logo.png";
+import { Link } from "react-router-dom";
+
+const footer = () => {
+  return (
+    <>
+      <div className="separator"></div>
+      <div className="footer-container">
+        <div className="degradent-content">
+          <div className="container-logo-footer">
+            <div className="logo-form">
+              <img src={logo} alt="" className="" />
+              <h1 className="txt-logo">mungTickets</h1>
+            </div>
+          </div>
+          <div className="container-contactanos-footer">
+            <h3 className="txt-contactanos">Contáctanos</h3>
+            <h3 className="txt-mail underline">SmungTickets@gmail.com</h3>
+          </div>
+
+          <div className="container-descarga-footer">
+            <h3 className="txt-contactanos">Descarga</h3>
+            <h3 className="txt-mail">Android</h3>
+          </div>
+          <div className="container-social-footer">
+            <h3 className="txt-contactanos">Síguenos en redes sociales</h3>
+            <div className="cont-social">
+              <Link to="https://www.facebook.com/">
+                <i class="fa-brands fa-facebook"></i>
+              </Link>
+              <Link to="/">
+                <i class="fa-brands fa-instagram"></i>
+              </Link>
+              <Link to="/">
+                <i class="fa-brands fa-twitter"></i>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default footer;
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import "./assets/home.css";
+import Footer from "../../components/footer/Footer";
 
 export const HomePage = () => {
   return (
@@ -14,7 +15,7 @@ export const HomePage = () => {
         <h1 className="title-page">Eventos próximos...</h1>
         <button className="btn-load-more">Cargar más eventos</button>
       </div>
-      <div>footer for the moment</div>
+      <Footer/>
     </>
   );
 };
