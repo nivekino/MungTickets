be93616475a1cf9a44d022f2c9d8d50cfef5f122
nivekino <00100719@uca.edu.sv>
2023-06-02 15:33:08 -0600

    update fix

---
+++ src/pages/pagoEvento/PagoEvento.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PagoEvento = () => {
+  return (
+    <>
+      <h1>PagoEvento</h1>
+    </>
+  );
+};
+
+export default PagoEvento;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navigation from "../components/navigation/Navigation";
 import { HomePage } from "../pages/homepage/HomePage";
+import PagoEvento from "../pages/pagoEvento/PagoEvento";
+import DetalleEvento from "../pages/detalleEvento/DetalleEvento";
 
 
 const AppRouter = () => {
@@ -10,6 +12,8 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<HomePage />} />
+          <Route path="pagoEvento" element={<PagoEvento />} />
+          <Route path="detalleEvento" element={<DetalleEvento />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
